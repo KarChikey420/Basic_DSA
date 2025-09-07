28365f9759e06c122db788a29bd9030ca152ec8f
KarChikey <kartikeynegi2002@gmail.com>
2025-09-07 16:44:04 +0530

    find duplicat in list

---
+++ find_duplicate.py
@@ -0,0 +1,12 @@
+#find the duplicate in the list
+
+def find_duplicate(arr):
+    seen=set()
+    for num in arr:
+        if num in seen:
+            return num
+        seen.add(num)
+    return None
+
+arr=[1,4,3,2,5,4,5]
+print(find_duplicate(arr))
