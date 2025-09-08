c7ecc58ba2de9480c8ee24ba41fba8ab5767a31c
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 19:44:04 +0530

    craete dict

---
+++ dict_question/craete.py
@@ -0,0 +1,13 @@
+def create_dictonary():
+    dict={
+        'maths':20,
+        'English':30,
+        'science':40
+    }
+    
+    return dict
+    
+printdict=create_dictonary()
+print(printdict)
+
+
