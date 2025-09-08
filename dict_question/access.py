5c1bf2021b14d233fe7b6556542e5b0cde2ce115
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 19:50:40 +0530

    access value

---
+++ dict_question/access.py
@@ -0,0 +1,8 @@
+# Access dictionary value:
+# Given a dictionary {"name": "John", "age": 25, "city": "New York"}, print the value of "city".
+
+dictionary={"name": "John", 
+            "age": 25,
+            "city": "New York"}
+            
+print(dictionary["city"])
