9caebd9d7708e8ca74c846931b616fde1795508a
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 21:06:53 +0530

    check

---
+++ dict_question/check.py
@@ -0,0 +1,9 @@
+# Check if key exists:
+# Write a program to check if "salary" exists in {"name": "Raj", "age": 28}.
+
+dictonary={"name": "Raj", "age": 28}
+
+if "salary" in dictonary:
+    print("exist")
+else:
+    print("not exist")
