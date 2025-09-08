ca6ab566200e65e14b55846ae962e197c69d86db
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 21:12:48 +0530

    looping

---
+++ dict_question/loop.py
@@ -0,0 +1,11 @@
+# Loop through dictionary:
+# Print all keys and values using a loop.
+
+dictionary={
+    "name":"kartikey",
+    "age":22,
+    "hieght":5.8
+}
+
+for i in dictionary:
+    print(i ,":",dictionary[i])
