bc1ca882b63708c876e6a2d32c5ffc5e48830dd8
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 19:55:21 +0530

    add

---
+++ dict_question/add_value.py
@@ -0,0 +1,9 @@
+# Add new key-value pair:
+# Start with student = {"name": "Alice", "age": 20} and add "grade": "A".
+
+dictonary={"name": "Alice",
+           "age": 20}
+
+dictonary["grade"]="A"
+
+print(dictonary)
