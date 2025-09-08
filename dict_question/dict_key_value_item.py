ae6b777f4e4103a370580dedd4304b049792bb6f
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 21:28:21 +0530

    key value items

---
+++ dict_question/dict_key_value_item.py
@@ -0,0 +1,16 @@
+# Get dictionary keys as list:
+# Convert the keys of {"x": 10, "y": 20, "z": 30} into a list.
+
+dictionary={"x": 10, "y": 20, "z": 30}
+
+dicto=list(dictionary.keys())
+
+dicto_value=list(dictionary.values())
+
+dicto_items=list(dictionary.items())
+
+print(dicto)
+
+print(dicto_value)
+
+print(dicto_items)
