83195d571a8d193f9836782d8afb0423b5ccc26f
KarChikey <kartikeynegi2002@gmail.com>
2025-09-11 17:48:03 +0530

    no code no hope

--- find_duplicate.py
+++ find_duplicate.py
@@ -10,3 +10,15 @@ def find_duplicate(arr):
 
 arr=[1,4,3,2,5,4]
 print(find_duplicate(arr))
+
+#second approach
+
+def duplicate(array):
+    duplicates = []
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] == array[j]:
+                duplicates.append(array[i])
+    return duplicates
+
+print(duplicate([2, 2, 3,3, 4, 5]))
