3988b15961d320a7b90217e95f763265a01e9d22
KarChikey <kartikeynegi2002@gmail.com>
2025-09-16 22:21:34 +0530

    selection

--- sorting/selection.py
+++ sorting/selection.py
@@ -2,10 +2,10 @@ def selection_sort(array):
     n=len(array)
     for i in range(n):
         min_index=i
-        for j in range(j+1,n):
-            if array[j]>array[min_index]:
+        for j in range(i+1,n):
+            if array[j]<array[min_index]:
                 min_index=j
-                array[i],array[min_index]=array[min_index],array[i]
+        array[i],array[min_index]=array[min_index],array[i]
     return array
 
 array=[6,7,4,5,2,3,1]
