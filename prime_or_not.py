4b52898e7cd5729242628cb63b95c39d62f70527
KarChikey <kartikeynegi2002@gmail.com>
2025-09-17 23:20:14 +0530

    add

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,13 +1,11 @@
 def prime_or_not(num):
-    if num<=1: 
-      return f"{num} not a prime number"
-      
-    for i in range(2,num):
-        if num%i==0:
-            return f"{num} not a prime number"
+  if num<=1:
+    return False
+  
+  for i in range(2,num):
+    if num%i==0:
+      return False
     
-    return f"{num} is prime number"
-    
+  return True
 
-num1=int(input("Enter a number: "))
-print(prime_or_not(num1))
+print(prime_or_not(5))
