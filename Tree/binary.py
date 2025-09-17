33a3c5f2d58d7a54b28fcf4794cbfe2fe1cb53bc
KarChikey <kartikeynegi2002@gmail.com>
2025-09-18 00:04:24 +0530

    rr

--- Tree/binary.py
+++ Tree/binary.py
@@ -1,21 +1,44 @@
 class Node:
-    def __init__(self,value,left,right):
-        self.value=value
+    def __init__(self,data):
+        self.data=data
         self.left=None
         self.right=None
-        
-class Binary_Tree:
-    def __init__(self):
-        self.root=None
-        
-    def insert(self,value):
-        if not self.root:
-            self.root=value
-        else:
-            self._insert(self.root,value)
-    
-    def insert(self,current,value):
-         
-    
 
-    
+def Binary_Tree(root,key):
+    if root is None:
+        return Node(key)
+    if key<root.data:
+        root.left=Binary_Tree(root.left,key)
+    else:
+        root.right=Binary_Tree(root.right,key)
+    return root
+
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data,end='')        
+        inorder(node.right)
+        
+def preoder(node):
+    if node:
+        print(node.data,end='')
+        preoder(node.left)
+        preoder(node.right)
+        
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data,end='')
+        
+number=[1,2,3,4,5,6,7,8]
+root=None
+for num in number:
+    root=Binary_Tree(root,num)
+
+print('inorder')
+inorder(root)
+print('\npreorder')
+preoder(root)
+print('\npostorder')
+postorder(root)
