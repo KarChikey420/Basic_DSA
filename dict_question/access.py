bab1691f7fcef9044cb1c3494167ef237b1e3f81
KarChikey <kartikeynegi2002@gmail.com>
2025-09-17 23:23:06 +0530

    add

--- dict_question/access.py
+++ dict_question/access.py
@@ -1,8 +1,5 @@
 # Access dictionary value:
 # Given a dictionary {"name": "John", "age": 25, "city": "New York"}, print the value of "city".
 
-dictionary={"name": "John", 
-            "age": 25,
-            "city": "New York"}
-            
-print(dictionary["city"])
+dict1={"name": "John", "age": 25, "city": "New York"}
+print(dict1["city"])
