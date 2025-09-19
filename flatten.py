90a3ad921fdf189a71a7d796b63a148a5e14e7e8
KarChikey <kartikeynegi2002@gmail.com>
2025-09-19 15:51:13 +0530

    nested list

--- flatten.py
+++ flatten.py
@@ -1,11 +1,13 @@
-def flatten(nested_list):
+def flatten(nasted_list):
     result=[]
-    for i in nested_list:
+     
+    for i in nasted_list:
         if isinstance(i,list):
             result.extend(flatten(i))
         else:
             result.append(i)
     return result
+
 
 nested = [[1, 2], [3, [4, 5]], 6]
 print(flatten(nested))
