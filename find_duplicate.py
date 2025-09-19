86aeda69d2c3d74d2aa0ee9ad459a349bfb40fee
KarChikey <kartikeynegi2002@gmail.com>
2025-09-19 23:35:51 +0530

    commit

--- find_duplicate.py
+++ find_duplicate.py
@@ -1,24 +1,36 @@
 #find the duplicate in the list
 
-def find_duplicate(arr):
-    seen=set()
-    for num in arr:
-        if num in seen:
-            return num
-        seen.add(num)
-    return None
+# def find_duplicate(arr):
+#     seen=set()
+#     for num in arr:
+#         if num in seen:
+#             return num
+#         seen.add(num)
+#     return None
 
-arr=[1,4,3,2,5,4]
-print(find_duplicate(arr))
+# arr=[1,4,3,2,5,4]
+# print(find_duplicate(arr))
 
 #second approach
 
-def duplicate(array):
-    duplicates = []
-    for i in range(len(array)):
-        for j in range(i+1, len(array)):
-            if array[i] == array[j]:
-                duplicates.append(array[i])
-    return duplicates
+# def duplicate(array):
+#     duplicates = []
+#     for i in range(len(array)):
+#         for j in range(i+1, len(array)):
+#             if array[i] == array[j]:
+#                 duplicates.append(array[i])
+#     return duplicates
 
-print(duplicate([2, 2, 3,3, 4, 5]))
+# print(duplicate([2, 2, 3,3, 4, 5]))
+
+#remove duplicates
+
+def remove_duplicate(arr):
+    result=[]
+    
+    for item in arr:
+        if item not in result:
+            result.append(item)
+    return result
+arr=[1,2,1,3,2,4]               
+print(remove_duplicate(arr))
