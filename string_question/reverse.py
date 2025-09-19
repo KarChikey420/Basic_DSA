d3d1c2f01bac3be1f49545abc44faa068d76bc21
KarChikey <kartikeynegi2002@gmail.com>
2025-09-19 22:20:18 +0530

    add

--- string_question/reverse.py
+++ string_question/reverse.py
@@ -1,15 +1,23 @@
-#using empty string 
+# #using empty string 
 
-char="python"
-char1=""
+# char="python"
+# char1=""
 
-for i in char:
-    char1=i+char1
+# for i in char:
+#     char1=i+char1
     
-print(char1)
+# print(char1)
 
-#using slicing 
+# #using slicing 
 
-char="python"
-char1=char[::-1]
-print(char1)
+# char="python"
+# char1=char[::-1]
+# print(char1)
+
+my_char='kartikey'
+char=''
+
+for i in my_char:
+    char=i + char
+    
+print(char)
