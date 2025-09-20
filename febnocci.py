76d7118c59475a48cfaf81420a95c01af69aa311
KarChikey <kartikeynegi2002@gmail.com>
2025-09-20 22:45:26 +0530

    ad

--- febnocci.py
+++ febnocci.py
@@ -1,7 +1,15 @@
+# num=10
+# feb=[0,1]
+
+# for i in range(2,num):
+#     feb.append(feb[i-1]+feb[i-2])
+    
+# print(feb)
+
 num=10
 feb=[0,1]
 
 for i in range(2,num):
     feb.append(feb[i-1]+feb[i-2])
-    
+
 print(feb)
