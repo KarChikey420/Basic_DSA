37277013471d5516b8023b655bc7e5482d32a747
KarChikey <kartikeynegi2002@gmail.com>
2025-09-30 17:47:00 +0530

    add

--- array/factorial.py
+++ array/factorial.py
@@ -1,31 +1,7 @@
-# def facto(num):
-#     fact=1
-#     if num==0 and num==1:
-#         return 1
-    
-#     for i in range(1,num+1):
-#         fact*=i
-        
-#     return fact
+def fectorial(num):
+    fact=1
+    for i in range(1,num+1):
+        fact*=i
+    return fact
 
-# print(facto(5))
-
-
-# def fact(num):
-#     fact=1
-#     if num==0 and num==1:
-#         return num
-    
-    
-#     for i in range(1,num+1):
-#         fact*=i
-#     return fact
-# print(fact(5))
-
-def fact(num):
-    if num == 0 or num == 1:  
-        return 1
-    else:
-        return num * fact(num - 1)
-
-print(fact(5)) 
+print(fectorial(5))
