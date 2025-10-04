164f0afba49cb4307b278749df340e1e55d6cca8
KarChikey <kartikeynegi2002@gmail.com>
2025-10-04 11:19:50 +0530

    add

--- array/binary.py
+++ array/binary.py
@@ -7,13 +7,12 @@ def binary_search(arr,target):
         
         if arr[mid]==target:
             return mid
-        
-        elif arr[mid]<target:
-            low=mid+1
-        else:
+        elif arr[mid]>target:
             high=mid-1
+        else:
+            low=mid+1
     return -1
 
-arr=[1,2,3,4,5,6,7,8,9]
+arr=[1,2,3,4,5,6]
 target=5
 print(binary_search(arr,target))
