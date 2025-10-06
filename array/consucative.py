dc47946fb7b34e43ced2770ca73b381651ada3bc
KarChikey <kartikeynegi2002@gmail.com>
2025-10-06 23:21:04 +0530

    add

--- array/consucative.py
+++ array/consucative.py
@@ -1,8 +1,10 @@
 def consecutive(arr):
     arr.sort()
     for i in range(len(arr)-1):
-        if i+1 not in arr:
-            return i
+        if arr[i+1]!=arr[i]+1:
+            return arr[i]+1
+        # if i+1 not in arr:
+        #     return i
     return "all elements are consecutive"
 
 arr=[1,2,3,5,6]
