4dceec36327123af84a67b6432303b62b2b68a5c
KarChikey <kartikeynegi2002@gmail.com>
2025-10-12 17:46:08 +0530

    add

--- array/binary.py
+++ array/binary.py
@@ -11,6 +11,8 @@ def binary(arr,target):
             low=mid+1
         else:
             high=mid-1
+            
     return -1
 
-print(binary([1,2,3,4,5,6],5))
+print(binary([1,2,3,4,5,6,7,8,9],5))
+        
