6781f0a01d943dfdd58c620cb8b315ed8a5ebde0
KarChikey <kartikeynegi2002@gmail.com>
2025-10-13 21:59:30 +0530

    add

--- array/secondlargest.py
+++ array/secondlargest.py
@@ -1,7 +1,5 @@
-def secondlar(list1):
-    list1.sort()
-    second = list1[-2]
-    return second
+def second_high(arr):
+    arr.sort()
+    return arr[-2]
 
-listis=[2,4,56,6,77]
-print(secondlar(listis))
+print(second_high([1,2,3,4,5,6,7,8,9]))
