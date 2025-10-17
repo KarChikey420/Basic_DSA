93746202317c6e7886e9759e75fbef4a525d6827
KarChikey <kartikeynegi2002@gmail.com>
2025-10-17 10:54:07 +0530

    add

--- array/find_duplicate.py
+++ array/find_duplicate.py
@@ -1,58 +1,19 @@
-#find the duplicate in the list
-
-# def find_duplicate(arr):
-#     seen=set()
-#     for num in arr:
-#         if num in seen:
-#             return num
-#         seen.add(num)
-#     return None
-
-# arr=[1,4,3,2,5,4]
-# print(find_duplicate(arr))
-
-#second approach
-
-# def duplicate(array):
-#     duplicates = []
-#     for i in range(len(array)):
-#         for j in range(i+1, len(array)):
-#             if array[i] == array[j]:
-#                 duplicates.append(array[i])
-#     return duplicates
-
-# print(duplicate([2, 2, 3,3, 4, 5]))
-
-#remove duplicates
-
-# def remove_duplicate(arr):
-#     result=[]
-    
-#     for item in arr:
-#         if item not in result:
-#             result.append(item)
-#     return result
-# arr=[1,2,1,3,2,4]               
-# print(remove_duplicate(arr))
-
-# def removed_dupli(arr):
-#     k=1
+# def duplicate(arr):
+#     arr.sort()
+#     dupli=[arr[0]]
 #     for i in range(1,len(arr)):
 #         if arr[i]!=arr[i-1]:
-#             arr[k]=arr[i]
-#             k+=1
-#         return k
+#             dupli.append(arr[i])
+#     return dupli
     
-# arr=[1,2,1,3,2,4]
-# k=removed_dupli(arr)
-# print(arr[:k])
+# print(duplicate([1,2,4,5,2,1,4]))
 
-def find_duplicate(arr):
+def duplicate(arr):
     arr.sort()
-    rm_list=[arr[0]]
+    dupli=[]
     for i in range(1,len(arr)):
-        if arr[i]!=arr[i-1]:
-            rm_list.append(arr[i])
-    return rm_list
-arr=[1,2,1,3,2,4]
-print(find_duplicate(arr))
+        if arr[i]==arr[i-1]:
+            dupli.append(arr[i])
+    return dupli
+    
+print(duplicate([1,2,4,5,2,1,4]))
