081b24e07b6fe87e3d5429d798473651529c1a81
KarChikey <kartikeynegi2002@gmail.com>
2025-10-17 09:53:17 +0530

    add

--- array/consucative.py
+++ array/consucative.py
@@ -1,17 +1,12 @@
-# def consecutive(arr):
-#     list1=[]
-#     for i in range(len(arr)-1):
-#         current=arr[i]
-#         next=arr[i+1]
-#         for n in range(current+1,next):
-#             list1.append(n)
-#     return list1
-
-# print(consecutive([1,3,5,7]))
-
-def consecutive(arr):
+def check_consucative(arr):
+    missing=[]
+    
     for i in range(len(arr)-1):
-        if arr[i+1]!=arr[i]+1:
-            return arr[i]+1
-
-print(consecutive([1,2,4,6,7,8]))
+        current=arr[i]
+        next_val=arr[i+1]
+        if current+1!=next_val:
+            for num in range(current+1,next_val):
+                   missing.append(num)
+    return missing
+    
+print(check_consucative([1,2,4,6,8]))
