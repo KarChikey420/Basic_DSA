d2d9f03cb69b9b87bf3c9d44d9ceb505a2f8d05c
KarChikey <kartikeynegi2002@gmail.com>
2025-10-17 10:34:23 +0530

    add

--- array/consucative.py
+++ array/consucative.py
@@ -1,12 +1,25 @@
-def check_consucative(arr):
-    missing=[]
+# def check_consacutive(arr):
+#     missing=[]
     
+#     for i in range(len(arr)-1):
+#         current=arr[i]
+#         next_val=arr[i+1]
+        
+#         if current+1!=next_val:
+#             for num in range(current+1,next_val):
+#                 missing.append(num)
+#     return missing
+    
+# arr=[1,3,5,7]
+# print(check_consacutive(arr))
+
+def check_consacutive(arr):
     for i in range(len(arr)-1):
         current=arr[i]
         next_val=arr[i+1]
+        
         if current+1!=next_val:
-            for num in range(current+1,next_val):
-                   missing.append(num)
-    return missing
-    
-print(check_consucative([1,2,4,6,8]))
+            return current+1
+            
+arr=[1,3,4,5]
+print(check_consacutive(arr))
