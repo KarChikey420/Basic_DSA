312106999b22b64bb8684957e44c9ebaecaf9f3f
KarChikey <kartikeynegi2002@gmail.com>
2025-10-18 13:28:57 +0530

    add

--- array/prime_or_not.py
+++ array/prime_or_not.py
@@ -1,13 +1,13 @@
 def prime_or_not(num):
-  if num<=1:
-      return False
-  
-  for i in range(2,num):
-      if num%i==0:
+    if num<=1:
         return False
     
-  if num%num==0:
-     return True
-   
-print(prime_or_not(1))
-    
+    for i in range(2,num):
+        if num%i==0:
+            return False
+            
+    return True
+
+for i in range(0,100):
+    if prime_or_not(i):
+        print(f"{i} is prime")
