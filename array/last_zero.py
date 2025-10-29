6695352cc260ca623b687c417a6a2a74295d0f6a
KarChikey <kartikeynegi2002@gmail.com>
2025-10-29 19:12:23 +0530

    add

--- array/last_zero.py
+++ array/last_zero.py
@@ -1,9 +1,22 @@
-def zero_last(arr):
-   j=0
-   for i in range(len(arr)):
-       if arr[i]!=0:
-           arr[i],arr[j]=arr[j],arr[i]
-           j+=1
-   return arr
+# def zero_last(arr):
+#    j=0
+#    for i in range(len(arr)):
+#        if arr[i]!=0:
+#            arr[i],arr[j]=arr[j],arr[i]
+#            j+=1
+#    return arr
     
-print(zero_last([1,0,2,0,3,0,4,0]))
+# print(zero_last([1,0,2,0,3,0,4,0]))
+
+def move_zero(arr):
+    arr1=[]
+    zero_count=0
+    for i in arr:
+        if i!=0:
+            arr1.append(i)
+        else:
+            zero_count+=1
+    arr1.extend([0]*zero_count)
+    return arr1
+
+print(move_zero([1,0,2,0,3,0]))
