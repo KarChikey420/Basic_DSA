2543b8ba12e0539af6f9bc6f14961092cc87b471
KarChikey <kartikeynegi2002@gmail.com>
2025-10-29 18:40:23 +0530

    add

--- array/factorial.py
+++ array/factorial.py
@@ -1,6 +1,8 @@
-def factorial(n):
+def factorial(num):
     fact=1
-    for i in range(1,n+1):
-        fact*=i
+    
+    for i in range(1,num+1):
+        fact=fact*i
     return fact
+    
 print(factorial(5))
