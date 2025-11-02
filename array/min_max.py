ca908a937f0e33955765bf054112139231d2f542
KarChikey <kartikeynegi2002@gmail.com>
2025-11-02 14:19:39 +0530

    add

--- array/min_max.py
+++ array/min_max.py
@@ -3,9 +3,8 @@ def min_max(arr):
     max=arr[0]
     for i in range(1,len(arr)):
         if arr[i]>max:
-            max=arr[i]
-        elif arr[i]<min:
+           max=arr[i]
+        else:
             min=arr[i]
     return max,min
-arr=[1,2,3,4,5]
-print(min_max(arr))
+print(min_max([1,2,3,4,5]))
