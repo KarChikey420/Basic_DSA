44a757ccdd5377372a05dd8bee5daa83de471c6e
KarChikey <kartikeynegi2002@gmail.com>
2025-11-02 14:55:12 +0530

    add

--- array/find_duplicate.py
+++ array/find_duplicate.py
@@ -8,12 +8,20 @@
     
 # print(duplicate([1,2,4,5,2,1,4]))
 
-def duplicate(arr):
-    arr.sort()
-    dupli=[]
-    for i in range(1,len(arr)):
-        if arr[i]==arr[i-1]:
-            dupli.append(arr[i])
-    return dupli
+# def duplicate(arr):
+#     arr.sort()
+#     dupli=[]
+#     for i in range(1,len(arr)):
+#         if arr[i]==arr[i-1]:
+#             dupli.append(arr[i])
+#     return dupli
     
-print(duplicate([1,2,4,5,2,1,4]))
+# print(duplicate([1,2,4,5,2,1,4]))
+
+def remove_duplicate(arr):
+    remove=[]
+    for i in arr:
+        if i not in remove:
+            remove.append(i)
+    return remove
+print(remove_duplicate([1,2,3,1,4,2]))
