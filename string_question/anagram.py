10f8b297cf1259bc3546daf89c35db048288ac1d
KarChikey <kartikeynegi2002@gmail.com>
2025-11-03 21:25:27 +0530

    add

--- string_question/anagram.py
+++ string_question/anagram.py
@@ -1,12 +1,21 @@
 from collections import Counter
 
-def anagram(str1,str2):
-    sort_str1=sorted(str1)
-    sort_str2=sorted(str2)
+def are_anagrams(str1,str2):
+    sorted_str1=sorted(str1)
+    sorted_str2=sorted(str2)
     
-    if Counter(sort_str1)==Counter(sort_str2):
+    if sorted_str1==sorted_str2:
         return True
     else:
         return False
+
+if __name__=="__main__":
+    string1="listen"
+    string2="silent"
     
-print(anagram('abc','cba'))
+    result=are_anagrams(string1,string2)
+    if result:
+        print(f"{string1} and {string2} are anagrams")
+    else:
+        print(f"{string1} and {string2} are not anagrams")
+        
