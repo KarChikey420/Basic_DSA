dbc794743cfb75da220a8d3a3205c863d5932365
KarChikey <kartikeynegi2002@gmail.com>
2025-11-03 21:44:17 +0530

    add

--- string_question/frequency.py
+++ string_question/frequency.py
@@ -1,10 +1,13 @@
-def get_freq(s):
-    freq={}
-    for i in s:
-        if i in freq:
-            freq[i]+=1
+def get_freq(str1):
+    freq_dict={}
+    for i in str1:
+        if i in freq_dict:
+            freq_dict[i]+=1
         else:
-            freq[i]=1
-    return freq
+            freq_dict[i]=1
+    return freq_dict
 
-print(get_freq("aaabbbccc"))
+if __name__=="__main__":
+    string="programming"
+    frequency=get_freq(string)
+    print(f"Character frequency in '{string}': {frequency}")
