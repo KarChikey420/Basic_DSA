899c20c5e66b37a33c2f5a0cce30d4b58843e81e
KarChikey <kartikeynegi2002@gmail.com>
2025-11-10 14:04:44 +0530

    add

--- array/reverse_array.py
+++ array/reverse_array.py
@@ -1,10 +1,11 @@
-def reverse(arr):
+def reversing_arr(arr):
     left=0
     right=len(arr)-1
-    while left<right:
+    
+    for _ in range(1,len(arr)-1):
         arr[left],arr[right]=arr[right],arr[left]
         left+=1
         right-=1
     return arr
 
-print(reverse([1,2,3,4,5]))
+print(reversing_arr([1,2,3,4,5]))
