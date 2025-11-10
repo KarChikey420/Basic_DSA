77480a88bf4cff93ac6a7ccccfd6a09e66bc7175
KarChikey <kartikeynegi2002@gmail.com>
2025-11-10 14:25:28 +0530

    add

--- array/merge_arr.py
+++ array/merge_arr.py
@@ -1,17 +1,26 @@
-def merge_arrays(arr1,arr2):
+def merge_arr(arr1,arr2):
+    merged=[]
     i=0
     j=0
-    merge=[]
-    
     while i<len(arr1) and j<len(arr2):
         if arr1[i]<arr2[j]:
-            merge.append(arr1[i])
-            i=i+1
+            merged.append(arr1[i])
+            i+=1
         else:
-            merge.append(arr2[j])
-            j=j+1
-    merge.extend(arr1[i:])
-    merge.extend(arr2[j:])
-    return merge
+            merged.append(arr2[j])
+            j+=1
+    
+    while i<len(arr1):
+        merged.append(arr1[i])
+        i+=1
+    while j<len(arr2):
+        merged.append(arr2[j])
+        j+=1
+    
+    return merged
 
-print(merge_arrays([1,3,5],[2,4,6]))
+arr1=[1,2,3]
+arr2=[3,4,5]
+
+merged=merge_arr(arr1,arr2)
+print(merged)
