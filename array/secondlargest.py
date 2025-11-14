ac25565b4010ddc6bad0c62a008c67592c3365f0
KarChikey <kartikeynegi2002@gmail.com>
2025-11-14 15:50:15 +0530

    add

--- array/secondlargest.py
+++ array/secondlargest.py
@@ -1,9 +1,6 @@
-# def second_larg(arr):
-#    return sorted(set(arr))[-2]
-# print(second_larg([1,2,3,4,5]))
-
 def second_largest(arr):
    first=second=0
+   
    for i in range(len(arr)):
       if arr[i]>first:
          second=first
@@ -12,4 +9,5 @@ def second_largest(arr):
          second=arr[i]
    return second
 
-print(second_largest([2,4,5,6,7,8]))
+print(second_largest([1,2,3,4,5]))
+         
