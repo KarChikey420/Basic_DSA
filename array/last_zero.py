e7ee457832fc0f333fd1a35bb56fdf6f83130d3b
KarChikey <kartikeynegi2002@gmail.com>
2025-11-17 10:35:50 +0530

    add

--- array/last_zero.py
+++ array/last_zero.py
@@ -1,9 +1,10 @@
-def move_zero(arr):
+def moveZeroes(nums):
     j=0
-    for i in range(len(arr)):
-        if arr[i]!=0:
-            arr[j],arr[i]=arr[i],arr[j]
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            nums[i],nums[j]=nums[j],nums[i]
             j+=1
-    return arr
+    return nums
 
-print(move_zero([0,0,1,2,4,5]))
+print(moveZeroes([0,1,0,3,12]))
+        
