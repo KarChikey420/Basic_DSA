8da94f2a56567e3ffacd1e8226f0d05ec11b1849
KarChikey <kartikeynegi2002@gmail.com>
2025-11-20 16:32:36 +0530

    add

---
+++ array/disapear.py
@@ -0,0 +1,8 @@
+def findDisappearedNumbers(nums):
+    dis=[]
+    for i in range(1,len(nums)+1):
+        if i not in nums:
+            dis.append(i)
+    return dis
+
+print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
