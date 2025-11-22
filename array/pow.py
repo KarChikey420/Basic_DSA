6d403fc89fdcab4b890d8bd64e66c1bccc456487
KarChikey <kartikeynegi2002@gmail.com>
2025-11-22 14:49:56 +0530

    add

---
+++ array/pow.py
@@ -0,0 +1,10 @@
+def myPow(x, n):
+        sqrt=1
+        power=abs(n)
+        for _ in range(power):
+            sqrt*=x
+        
+        if n<0:
+            return 1/sqrt
+        return sqrt
+print(myPow(2.00000, -2))
