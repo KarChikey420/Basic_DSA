6fdb170403a22166d34acd31a1e2509e65d48736
KarChikey <kartikeynegi2002@gmail.com>
2025-12-01 15:13:10 +0530

    add

--- string_question/palindrom.py
+++ string_question/palindrom.py
@@ -8,6 +8,19 @@ def palindrom(str1):
     else:
         return False
 
+def isPalindrome(s):
+        char = "".join(c.lower() for c in s if c.isalnum())
+        n=len(char)
+        l=0
+        r=n-1
+
+        while l<r:
+            if char[l]!=char[r]:
+               return False
+            l+=1
+            r-=1
+        return True
+
 str1="lebel"
 if palindrom(str1):
     print("Palindrom")
