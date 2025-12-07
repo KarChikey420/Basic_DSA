15ede86c033a06051ceaa5059cb96a2e178637a7
KarChikey <kartikeynegi2002@gmail.com>
2025-12-07 14:04:14 +0530

    add

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,11 +1,16 @@
 def selection_sort(arr):
-    for i in range(len(arr)):
+    n=len(arr)
+    
+    for i in range(n):
         min_index=i
-        for j in range(i+1,len(arr)):
+        for j in range(i+1,n):
             if arr[j]<arr[min_index]:
                 min_index=j
-                arr[i],arr[min_index]=arr[min_index],arr[i]
+        arr[i],arr[min_index]=arr[min_index],arr[i]
     return arr
-
-arr=selection_sort([2,1,3,5,6])
+    
+arr=[1,3,2,5,4]
 print(selection_sort(arr))
+            
+        
+        
