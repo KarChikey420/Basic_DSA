a5c6a3c354671b307068c0cce6d751144c7bfe49
KarChikey <kartikeynegi2002@gmail.com>
2025-12-21 19:54:04 +0530

    add

--- array/febnocci.py
+++ array/febnocci.py
@@ -1,15 +1,16 @@
-# def febbo(num):
-#     feb=[0,1]
-#     for i in range(2,num):
-#         feb.append(feb[i-1]+feb[i-2])
-#     return feb
-# print(febbo(10))
+def febbo(num):
+    feb=[0,1]
+    for i in range(2,num):
+        feb.append(feb[i-1]+feb[i-2])
+    return feb
 
-def febbnocci(num):
-    a,b=0,1
-    for _ in range(num):
-        yield a
-        a,b=b,a+b
+print(febbo(10))
+
+# def febbnocci(num):
+#     a,b=0,1
+#     for _ in range(num):
+#         yield a
+#         a,b=b,a+b
         
-for num in febbnocci(10):
-    print(num)
+# for num in febbnocci(10):
+#     print(num)
