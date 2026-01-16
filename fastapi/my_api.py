124f918528426129ace5ea5e94854d1a364b58ee
KarChikey <kartikeynegi2002@gmail.com>
2026-01-16 22:08:43 +0530

    get&post

--- fastapi/my_api.py
+++ fastapi/my_api.py
@@ -1,1 +1,24 @@
-from fast
+from fastapi import FastAPI
+from database import sessionmaker, engine, Base
+from model import User
+
+app=FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.post("/users/")
+def create_user(user:User):
+    db=sessionmaker()
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+    return user 
+
+@app.get("/users/{user_id}")
+def read_user(user_id:int):
+    db=sessionmaker()
+    user=db.query(User).filter(User.id==user_id).first()
+    db.close()
+    return user
+
