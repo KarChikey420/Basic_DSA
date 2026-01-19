e8fee11839fbf8bda1ab8f4cee9ca51ad51dcdbe
KarChikey <kartikeynegi2002@gmail.com>
2026-01-19 22:34:27 +0530

    add

--- Tree/binary.py
+++ Tree/binary.py
@@ -3,38 +3,17 @@ class Node:
         self.data=data
         self.left=None
         self.right=None
-         
-def inorder(root):
-    if root is None:
-        return
-    inorder(root.left)
-    print(root.data,end="")
-    inorder(root.right)
-    return root
 
-def preorder(root):
-    if root is None:
-        return
-    preorder(root.left)
-    preorder(root.right)
-    print(root.data, end="")
-    return root
-
-def postorder(root):
-    if root is None:
-        return
-    print(root.data, end="")
-    postorder(root.left)
-    postorder(root.right)
-    return root     
-
-root=Node(1)
-root.left=Node(2)
-root.right=Node(3)
-root.left.left=Node(4)
-root.left.right=Node(5)
-root.right.left=Node(6)
-
-inorder(root)
-preorder(root)
-postorder(root)
+class Tree:
+    def create_node(self,data):
+        return Node(data)
+    
+    def insert_node(self,root,data):
+        if root is None:
+            return self.create_node(data)
+        
+        if data < root.data:
+            root.left=self.insert_node(root.left,data)
+        else:
+            root.right=self.insert_node(root.right,data)
+        return root
